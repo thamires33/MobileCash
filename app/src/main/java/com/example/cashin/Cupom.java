63cb26ea09cd931ece80563b0df26e4bb5c2144c
thamires33 <http.thami@gmail.com>
2023-10-10 20:06:00 -0300

    First commit

---
+++ app/src/main/java/com/example/cashin/Cupom.java
@@ -0,0 +1,14 @@
+package com.example.cashin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cupom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cupom);
+    }
+}
